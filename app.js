26282ac96a2afc1a65562550852b12d8f508f8cd
Johan Nino <johanfnr@gmail.com>
2023-03-31 11:48:52 +0200

    1 Apariencia

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+let button = document.getElementById("buttom_deseo_nuevo");
+
+button.addEventListener("click", function() {
+  alert("Haz hecho clic en el botón!");
+});
+
+  function agregarDeseoNuevo() {
+    var formDeseo = document.getElementById("form_deseo");
+    var deseo_input = formDeseo.elements['deseo_input'].value
+    var listaDeseo = document.getElementById('lista_deseos_nuevos');
+    var nuevoElemento = document.createElement("li");
+    nuevoElemento.innerText = deseo_input;
+    listaDeseo.appendChild(nuevoElemento);
+  }
+
+  function agregarDeseoNuevo() {
+    var formDeseo = document.getElementById("form_deseo");
+    var deseo_input = formDeseo.elements['deseo_input'].value
+    var listaDeseo = document.getElementById('lista_deseos_nuevos');
+    var nuevoElemento = document.createElement("li");
+    nuevoElemento.innerText = deseo_input;
+    listaDeseo.appendChild(nuevoElemento);
+  }
+
+
